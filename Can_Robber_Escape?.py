9f820ff802b761035825a1b00239bb0aef94ef48
Sindhu12lll <96376069+Sindhu12lll@users.noreply.github.com>
2023-07-17 14:42:18 +0530

    n=int(input()) c=0 a=list(map(int,input().split())) for i in range(n):     if(a[i]%2!=0):         c=c+1 if c<=2:     print("YES") else:     print("NO")

--- Can_Robber_Escape?.py
+++ Can_Robber_Escape?.py
@@ -1,9 +1,9 @@
 n=int(input())
 c=0
 a=list(map(int,input().split()))
-for i in range(len(a)):
+for i in range(n):
     if(a[i]%2!=0):
-        c+=1
+        c=c+1
 if c<=2:
     print("YES")
 else:
