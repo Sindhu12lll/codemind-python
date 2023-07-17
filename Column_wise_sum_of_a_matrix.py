78612c5817ba527546dfc7380ce1def82ed117df
Sindhu12lll <96376069+Sindhu12lll@users.noreply.github.com>
2023-07-17 13:49:55 +0530

    n,m=map(int,input().split()) e=[] for i in range(n):     b=list(map(int,input().split()))     e.append(b) rsum=[] for i in range(m):     c=0     for j in range(n):         c+=e[j][i]     rsum.append(c) print(*rsum)

---
+++ Column_wise_sum_of_a_matrix.py
@@ -0,0 +1,12 @@
+n,m=map(int,input().split())
+e=[]
+for i in range(n):
+    b=list(map(int,input().split()))
+    e.append(b)
+rsum=[]
+for i in range(m):
+    c=0
+    for j in range(n):
+        c+=e[j][i]
+    rsum.append(c)
+print(*rsum)
